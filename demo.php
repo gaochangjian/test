645383a49e33bfc474cd5873ee4dad943b0fa1ca
高长健 <1002145342@qq.com>
2018-11-06 12:41:48 +0800

    测试testcomposer

---
+++ demo.php
@@ -0,0 +1,4 @@
+<?php
+require_once './vendor/autoload.php';
+use Gaochangjian\Test\Util;
+Util::hello();
---
+++ gaochangjian/Test/Util.php
@@ -0,0 +1,15 @@
+<?php
+namespace Gaochangjian\Test;
+
+class Util
+{
+	public function __construct()
+	{
+
+	}
+
+	public static function hello()
+	{
+		echo "hello word";
+	}
+}
